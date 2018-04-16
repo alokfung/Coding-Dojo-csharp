bd6e6d76a47a3f219e3952d6e0cc1c11359edf37
alokfung <alokfung@gmail.com>
2018-04-15 18:28:30 -0700

    first commit

---
+++ first_csharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a loop that prints all values from 1-255
+            Console.WriteLine("---- Question 1 ----");
+            for (int i = 1 ;i <=255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+            Console.WriteLine("---- Question 2 ----");
+            for (int i = 1; i <=100; i++)
+            {
+                if ((i%3==0 || i%5==0) && i%15!=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // Modify the previous loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+            Console.WriteLine("---- Question 3 ----");
+            for (int i = 1; i <=100; i++)
+            {
+                if (i%3==0 && i%15!=0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5==0 && i%15!=0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i%15==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+            }
+        }
+    }
+}
