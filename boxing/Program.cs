ba2c00a931c9a649ae431f4b80d69d79e1611946
alokfung <alokfung@gmail.com>
2018-04-18 14:51:23 -0700

    first commit

--- boxing/Program.cs
+++ boxing/Program.cs
@@ -7,7 +7,34 @@ namespace boxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an empty list of type object
+            List<object> alist = new List<object>();
+
+            // Add the following values to the list: 7, 28, -1, true, "chair" 
+            alist.Add(7);
+            alist.Add(28);
+            alist.Add(-1);
+            alist.Add(true);
+            alist.Add("chair");
+
+            // Loop through the list and print all values (Hint: Type Inference might help here!)
+            foreach (object i in alist)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Add all values that are Int type together and output the sum
+            int sum = 0;
+            foreach (object i in alist)
+            {
+                if(i is int)
+                {   
+                    int j = 0;
+                    j = (int)i;
+                    sum = sum + j;
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
